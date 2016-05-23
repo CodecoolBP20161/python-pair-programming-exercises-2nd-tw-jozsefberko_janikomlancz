55995f67bb5793f49b0c8c0f000173c0e70b2829
Komlancz Janos <jkomlancz@gmail.com>
2016-05-23 13:12:30 +0200

    Done

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,18 @@
 def fizzbuzz(number):
-    return
+    if number != 0:
+        if number % 15 == 0:
+            number = "FizzBuzz"
+        elif number % 3 == 0:
+            number = "Fizz"
+        elif number % 5 == 0:
+            number = "Buzz"
+    return number
 
 
 def main():
+    maximum_number = int(input("Please give a maximum number:"))
+    for num in range(maximum_number+1):
+        print(fizzbuzz(num))
     return
 
 if __name__ == '__main__':
