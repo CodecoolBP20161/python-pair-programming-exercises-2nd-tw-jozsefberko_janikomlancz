6910c31aa68cee93aa9e89d2681aa37d6bed7443
Komlancz Janos <jkomlancz@gmail.com>
2016-05-23 14:19:43 +0200

    Done

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,8 +1,18 @@
+import random
 def listoverlap(list1, list2):
-    return
-
+    common_list = (set(list1) & set(list2))
+    if len(common_list) == 0:
+        common_list = "There are not any common number(s) in the lists!"
+    return list(common_list)
 
 def main():
+    a = []
+    b = []
+    for i in range(random.randint(0, 11)):
+        a.append(random.randint(0, 100))
+        b.append(random.randint(0, 100))
+    print("A list = %s\nB list = %s"%(a, b))
+    print(listoverlap(a, b))
     return
 
 
