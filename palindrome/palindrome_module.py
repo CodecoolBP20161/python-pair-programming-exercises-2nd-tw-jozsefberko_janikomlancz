33e5e0125af713382a40dcb7246dd4c96ccfdb7c
Komlancz Janos <jkomlancz@gmail.com>
2016-05-25 09:34:21 +0200

    Done

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,19 @@
-def palindrome(str):
-    return
+
+def palindrome(stri):
+    stri = stri.replace(" ", "")
+    reverse = str(stri[::-1])
+    print(reverse)
+    if stri.lower() == reverse.lower():
+        print('That is a palindrome!')
+        return True
+    else:
+        print('That is not a palindrome!')
+        return False
 
 
 def main():
+    stri = input('Enter a word to check: ')
+    palindrome(stri)
     return
 
 
